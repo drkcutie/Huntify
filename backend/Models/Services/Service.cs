26e9d6c2c351fe014fef27950b08095d01081044
drkcutie <derrick.binangbang@cit.edu>
2024-12-09 17:31:33 +0800

    add description and rate in providerservice

--- backend/Models/Services/Service.cs
+++ backend/Models/Services/Service.cs
@@ -13,12 +13,16 @@ public class Service
     [Key]
     public int ServiceId { get; set; }
     
+    
     public string ServiceType { get; set; } = "General";
 
     [ServiceUnique]
+    [MaxLength(255)]
     public string Title { get; set; } = "";
-
+    
+    [MaxLength(255)]
     public string Description { get; set; } = "";
+    [MaxLength(255)]
     public string Image { get; set; } = "";
 
     // Relationships
