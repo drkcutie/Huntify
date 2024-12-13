1cb07b7c10d83aded218b5dac0df01791111b8f2
drkcutie <derrick.binangbang@cit.edu>
2024-12-13 20:43:40 +0800

    Add post for provider service

--- backend/Models/Services/ProviderService.cs
+++ backend/Models/Services/ProviderService.cs
@@ -6,36 +6,56 @@ namespace backend.Models.Services;
 
 public class ProviderService
 {
- [Key]
- public int ProviderServiceId { get; set; }
- 
- public int ServiceProviderId { get; set; }
- 
- public ServiceProviderModel? ServiceProvider { get; set; } = null!;
- 
- 
- public int ServiceId { get; set; }
- 
- public Service.Service  Service{ get; set; } = null!;
+    [Key] public int ProviderServiceId { get; set; }
+
+    public int ServiceProviderId { get; set; }
+
+    public ServiceProviderModel? ServiceProvider { get; set; } = null!;
 
 
- public double Rate { get; set; }
- 
- public RateType RateType;
- [MaxLength(255)]
- public string Description { get; set; } = null!;
- 
- public  Experience YearsOfExperience { get; set; }
+    public int ServiceId { get; set; }
+    public Service.Service Service { get; set; } = null!;
 
+
+    public double Rate { get; set; }
+
+    public RateType RateType { get; set; }
+    [MaxLength(255)] public string Description { get; set; } = null!;
+
+    public Experience YearsOfExperience { get; set; }
+}
+
+public class PostProviderServiceDto
+{
+    public PostProviderServiceDto(string description)
+    {
+        Description = description;
+    }
+
+    [Required]
+    public int ServiceProviderId { get; set; }
+    [Required]
+    public int ServiceId { get; set; }
+    [Required]
+    public double Rate { get; set; }
+    [Required]
+    public RateType RateType { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public Experience YearsOfExperience { get; set; }
 }
 
 public enum Experience
 {
- LessThanOneYear, OneToThreeYears, ThreeToFiveYears, MoreThanFiveYears
+    LessThanOneYear,
+    OneToThreeYears,
+    ThreeToFiveYears,
+    MoreThanFiveYears
 }
-
 
 public enum RateType
 {
- PerHour, PerOrder
+    PerHour,
+    PerOrder
 }
